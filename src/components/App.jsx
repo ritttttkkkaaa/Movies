50010e38dd58746eb8644b55c92e785346c7439a
ritttttkkkaaa <mo659835@gmail.com>
2023-11-23 15:51:30 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import {  Route , Routes } from 'react-router-dom';
 import { lazy  } from 'react';
 const Home = lazy(() => import('../pages/Home/Home'));
 const Movies = lazy(() => import('../pages/Movie/Movies'));
-const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'))
-const Layout = lazy(() => import('../components/Layout/Layout'))
-const Cast = lazy(() => import('../components/Cast/Cast'))
-const Reviews = lazy(() => import('../components/Review/Review'))
+const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
+const Layout = lazy(() => import('../components/Layout/Layout'));
+const Cast = lazy(() => import('../components/Cast/Cast'));
+const Reviews = lazy(() => import('../components/Review/Review'));
 
 
 
